1abc9223c3a8d44908d872b4083fd235f338c939
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-14 22:41:11 -0300

    feat: create initial landing page components and layout

---
+++ src/components/Separator.tsx
@@ -0,0 +1,7 @@
+export default function Separator() {
+  return (
+    <div aria-hidden className="mx-auto max-w-7xl px-6">
+      <div className="my-1 h-px w-full bg-gradient-to-r from-transparent via-black/10 to-transparent" />
+    </div>
+  );
+}
